01674fb69d719bf6371fc1f18eac698d059bae0a
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2024-06-04 12:50:57 +0300

    Set Some Routes in UsersRoutes

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,8 +4,10 @@ const usersController = require("../controllers/usersController");
 const router = Router();
 
 //Get Routes
+router.get("/all", usersController.getAll);
 router.get("/:email", usersController.getByEmail);
 router.get("/token/:token", usersController.getByToken);
 router.post("/add", usersController.addUser);
+router.post("/update", usersController.UpdateUserSubscriptionIdById);
 
 module.exports = router;
