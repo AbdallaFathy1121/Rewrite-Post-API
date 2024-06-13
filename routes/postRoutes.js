da07f5c7f3d388ac0b6f5b7a87a1b0ce6db241d4
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2024-06-13 19:21:01 +0300

    Add JWT MiddleWares in Route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const postController = require("../controllers/postController");
+const jwtMiddleWares = require('../middlewares/jwt');
 
 const router = Router();
 
 // Post Routes
-router.post("/add", postController.AddPost);
+router.post("/add", jwtMiddleWares.authenticateJWT, postController.AddPost);
 
 module.exports = router;
